a25e15de25c6b3620037d05393936350f76c4c05
Vadim Psariov <Vadim.Psariov@endava.com>
2022-04-21 15:15:06 +0300

    wild card removed from import

--- src/test/java/model/NewMethodTest.java
+++ src/test/java/model/NewMethodTest.java
@@ -1,10 +1,9 @@
 package model;
 
 import org.junit.jupiter.api.Test;
-
 import java.io.InvalidObjectException;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NewMethodTest {
 
--- src/test/java/model/SavingsAccountTest.java
+++ src/test/java/model/SavingsAccountTest.java
@@ -1,8 +1,8 @@
 package model;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class SavingsAccountTest {
 
